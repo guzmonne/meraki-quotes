e47d56986b2362b0a31b66743f21f5d3c01f5b81
Guzman Monne <guzmonne@hotmail.com>
2016-03-09 14:26:23 -0300

    added mdb

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+require('./styles/bootstrap/css/bootstrap.css');
+require('./styles/mdb/css/mdb.css');
+require('./styles/main.scss');
+
 import React from 'react'
 import {render} from 'react-dom'
 import MainApp from './router.js'
