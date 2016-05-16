104a5385f5e735f03bfbb1a47c735b28535f65c1
Guzman Monne <guzmonne@hotmail.com>
2016-05-16 18:40:45 -0300

    added a Panel to the Users create form component

--- src/components/users/user-create-container.component.js
+++ src/components/users/user-create-container.component.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import {Grid, Row, Col} from 'react-bootstrap'
+import {Grid, Row, Col, Panel} from 'react-bootstrap'
 //import UserCreateForm from './user-create-form.component.js'
 import SignupForm from '../main/signup-form.component.js'
 
 export default (props) =>
 	<Grid className="UserCreate">
 		<Row>
-			<Col md={6} mdOffset={3}>
-				{(typeof props.signup.signupFormError === 'string' && !!props.signup.signupFormError) ? 
-					<div className="alert alert-danger">
-						{props.signup.signupFormError}
-					</div>:
-					null}
-					<SignupForm {...props} />
-			</Col>
+				
+				<Col md={6} mdOffset={3}>
+					<Panel>
+						{(typeof props.signup.signupFormError === 'string' && !!props.signup.signupFormError) ? 
+							<div className="alert alert-danger">
+								{props.signup.signupFormError}
+							</div>:
+							null}
+							<SignupForm {...props} />
+					</Panel>
+				</Col>
+				
 		</Row>
 	</Grid>
